ed530d2d48901dfbd8031a17cff6b72e40751c64
Minsuk-Kim <msk2361@naver.com>
2025-10-22 22:43:19 +0900

    502오류 수정 : urls 수정

--- app/config/urls.py
+++ app/config/urls.py
@@ -24,8 +24,7 @@ def api_health(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/health/", api_health, name="api_health"),
-    #index page - My Portfolio
     path("", include(("portfolio.urls", "portfolio"), namespace="portfolio")),
-    path("api/", include(("projects.urls", "projects"), namespace="projects")), # Project API
-    path("projects/minist/", include(("project_minist.urls", "project_minist"), namespace="project_minist")),
+    path("api/", include(("projects.urls", "projects"), namespace="projects")),  # Project API
+    path("projects/mnist/", include(("project_mnist.urls", "project_mnist"), namespace="project_mnist")),
 ]
